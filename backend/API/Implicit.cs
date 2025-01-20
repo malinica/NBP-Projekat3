938c21d60b86281e40aff4308cad5edd1a36bf49
unknown <malinovicjovana13@gmail.com>
2025-01-20 12:16:30 +0100

    Napravljen projekat i backend za rad sa swagger-om

---
+++ backend/API/Implicit.cs
@@ -0,0 +1,5 @@
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Text.Json.Serialization;
